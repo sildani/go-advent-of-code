4ea916616c773bbdb60dd40d7d44d542ae03fa11
Daniel Silva <daniel@silvanolte.com>
2021-05-12 17:57:27 -0400

    moved around files to pave way for future solutions

--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "strings"
-  "strconv"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 
-  "github.com/sildani/go-advent-of-code/2020"
+	"github.com/sildani/go-advent-of-code/2020/one"
 )
 
 func check(e error) {
-  if e != nil {
-    panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func convertByteSliceToIntSlice(bytes []byte) []int {
-  stringInput := strings.Split(string(bytes), "\n")
-  var retval []int
-  for _, str := range stringInput {
-    x, e := strconv.Atoi(strings.Replace(str, "\r","",-1))
-    check(e)
-    retval = append(retval, x)
-  }
-  return retval
+	stringInput := strings.Split(string(bytes), "\n")
+	var retval []int
+	for _, str := range stringInput {
+		x, e := strconv.Atoi(strings.Replace(str, "\r", "", -1))
+		check(e)
+		retval = append(retval, x)
+	}
+	return retval
 }
 
 func dayOne() {
-  dat, err := ioutil.ReadFile("./2020/input/one.txt")
-  check(err)
-  input := convertByteSliceToIntSlice(dat)
-  fmt.Printf("one.Eval: %v\n", one.Eval(input))
-  fmt.Printf("one.EvalPartTwo: %v\n", one.EvalPartTwo(input))
+	dat, err := ioutil.ReadFile("./2020/one/input/one.txt")
+	check(err)
+	input := convertByteSliceToIntSlice(dat)
+	fmt.Printf("one.Eval: %v\n", one.Eval(input))
+	fmt.Printf("one.EvalPartTwo: %v\n", one.EvalPartTwo(input))
 }
 
 func main() {
-  dayOne()
+	dayOne()
 }
