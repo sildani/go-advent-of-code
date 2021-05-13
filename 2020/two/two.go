3042468e5bca7ac5b64cec6d13783a170072263d
Daniel Silva <daniel@silvanolte.com>
2021-05-12 20:12:26 -0400

    added more tests and functionality

--- 2020/two/two.go
+++ 2020/two/two.go
@@ -3,6 +3,7 @@ package two
 import (
   "fmt"
   "strings"
+  "regexp"
 )
 
 func ParseData(data []byte) map[string][]string {
@@ -16,4 +17,18 @@ func ParseData(data []byte) map[string][]string {
   }
   fmt.Printf("retval: %v;\n", retval)
   return retval
+}
+
+func GeneratePolicyRegex(policy string) string {
+  x := strings.Split(policy, " ")
+  y := strings.Split(x[0], "-")
+  fmt.Printf("GeneratePolicyRegex; x: %s; y: %s;\n", x, y)
+  return fmt.Sprintf("%s{%s,%s}", x[1], y[0], y[1])
+}
+
+func PasswordAudit(policy string, password string) bool {
+  policyRegex := GeneratePolicyRegex(policy)
+  matched, _ := regexp.MatchString(policyRegex, password)
+  fmt.Printf("PasswordAudit; policyRegex: %s; password: %s; matched: %v;\n", policyRegex, password, matched)
+  return matched
 }
--- 2020/two/two_test.go
+++ 2020/two/two_test.go
@@ -16,6 +16,18 @@ func assertStringSlices(expected []string, actual []string, t *testing.T) {
     t.Errorf("expected %v, got %v\n", expected, actual)
   }
 }
+
+func assertStrings(expected string, actual string, t *testing.T) {
+  if expected != actual {
+    t.Errorf("expected %v, got %v\n", expected, actual)
+  }
+}
+
+func assertBooleans(expected bool, actual bool, t *testing.T) {
+  if expected != actual {
+    t.Errorf("expected %v, got %v\n", expected, actual)
+  }
+}
 func TestParseData(t *testing.T) {
   data := []byte("1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc")
   parsedData := ParseData(data)
@@ -42,4 +54,16 @@ func TestParseDataSameRuleTwice(t *testing.T) {
 
   assertInts(1, len(parsedData), t)
   assertStringSlices([]string{"abcde", "cdefg"}, parsedData["1-3 a"], t)
+}
+
+func TestGeneratePolicyRegex(t *testing.T) {
+  assertStrings("a{1,3}", GeneratePolicyRegex("1-3 a"), t)
+  assertStrings("b{1,3}", GeneratePolicyRegex("1-3 b"), t)
+  assertStrings("c{2,9}", GeneratePolicyRegex("2-9 c"), t)
+}
+
+func TestPasswordValid(t *testing.T) {
+  assertBooleans(true, PasswordAudit("1-3 a", "abcde"), t)
+  assertBooleans(false, PasswordAudit("1-3 b", "cdefg"), t)
+  assertBooleans(true, PasswordAudit("2-9 c", "ccccccccc"), t)
 }
